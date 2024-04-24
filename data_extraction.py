6972fab30afc48b108399280a67f74cb7047a77c
lyhongkhanh <lyhongkhanh101102@gmail.com>
2024-04-24 11:01:16 +0700

    Update data_extraction: fix capture image in the video capture iteration

--- data_extraction.py
+++ data_extraction.py
@@ -1,3 +1,4 @@
+#%%
 import cv2
 import os
 import time
@@ -42,26 +43,24 @@ def extract_face(pixels, required_size=(320, 320)):
 video_capture = cv2.VideoCapture(0)
 image_counter = 0
 dataset_dir = 'dataset'
-project_path = 'D:\Workspace\AI\Deep\final_project'
-dataset_path = 'D:\Workspace\AI\Deep\final_project\dataset'
+project_path = 'D:/Workspace/AI/Deep/final_project'
+dataset_path = 'D:/Workspace/AI/Deep/final_project/dataset'
 if not os.path.exists(dataset_dir):
     os.mkdir(dataset_dir)
 
+face_id = input('\n Make sure the first user entered is 0.enter user id end press <return> ==>  ')
+face_name = input(f'\n Give the name for {face_id} <return> ==>  ')
+print("\n [INFO] Initializing face capture. Look the camera and wait ...")
+label_file_path = os.path.join(project_path, 'labels.txt')
+# Capture frame-by-frame
+
+with open(label_file_path, 'a') as file:
+    if os.path.exists(label_file_path):
+        file.write('\n')
+    text_to_append = f'{face_id} {face_name}'
+    file.write(text_to_append)
 
 while True:
-    face_id = input('\n Make sure the first user entered is 0.enter user id end press <return> ==>  ')
-    face_name = input(f'\n Give the name for {face_id} <return> ==>  ')
-    print("\n [INFO] Initializing face capture. Look the camera and wait ...")
-    label_file_path = os.path.join(project_path, 'labels.txt')
-    # Capture frame-by-frame
-
-    with open(label_file_path, 'a') as file:
-        if os.path.exists(label_file_path):
-            file.write('\n')
-
-        text_to_append = face_name
-        file.write(text_to_append)
-
     ret, frame = video_capture.read()
 
     # Convert the image from BGR color (which OpenCV uses) to RGB color
@@ -74,17 +73,18 @@ while True:
     if face_array is not None:
         cv2.imshow('Video', cv2.cvtColor(face_array, cv2.COLOR_RGB2BGR))
         # Save the captured image to the dataset directory
-        image_path = os.path.join(dataset_dir, f'image_User_'+ str(face_id)+'.'+{image_counter}+'.jpg')
-        cv2.imwrite(dataset_path, cv2.cvtColor(face_array, cv2.COLOR_RGB2BGR))
+        image_path = os.path.join(dataset_dir, f'image_User_{face_id}_{image_counter}.jpg')
+        cv2.imwrite(image_path, cv2.cvtColor(face_array, cv2.COLOR_RGB2BGR))
+
         # Increment the image counter
         image_counter += 1
         # Check if captured enough images
-        if image_counter >= 70:
+        if image_counter >= 20:
             break
     else:
         cv2.imshow('Video', frame)
 
-    k = cv2.waitKey(25) & 0xFF
+    k = cv2.waitKey(50) & 0xFF
     if k == 27:  # press 'ESC' to quit
         break
 
@@ -96,3 +96,5 @@ cv2.destroyAllWindows()
 
 
 
+
+# %%
